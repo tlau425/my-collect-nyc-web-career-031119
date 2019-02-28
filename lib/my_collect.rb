05bc1ed35ee82e6a6597df27a1d1e92a1e046ee0
Tommy Lau <tlau425@yahoo.com>
2019-02-28 02:50:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,9 @@ collection = ['ruby', 'javascript', 'python', 'objective-c']
 
 def my_collect(array)
   i = 0
+  new_array = []
   while(i < array.length)
-    yield(array[i])
+    new_array << yield(array[i])
     i +=1
   end
 end
