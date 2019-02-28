cfc5a800215dc295db6e5bd4f07584322f9f6da5
Tommy Lau <tlau425@yahoo.com>
2019-02-28 02:55:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
+def my_collect(array)
+  i = 0
+  new_array = []
+  while(i < array.length)
+    new_array << yield(array[i])
+    i +=1
+  end
+  new_array
+end
 
+my_collect(collection) {|x| x.upcase}
